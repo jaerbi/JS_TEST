bf8e71f744c0ab320efa47cebe85590aa852b66d
Yura Avramenko <jaerbi42@gmail.com>
2021-12-01 12:21:02 +0200

    Update yashchenko.ts

--- js/yashchenko.ts
+++ js/yashchenko.ts
@@ -14,7 +14,7 @@ import { Component } from "@angular/core";
                 </li>
             </ul>
         </div>
-',
+', 
     styleUrls: ['./yashchenko.component.scss']
 })
 export class YashchenkoComponent {
